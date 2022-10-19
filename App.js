63f58f6fb8d5592f8a7aeda2e99fe449b995e754
Hussein Omar <husseinomar6190@gmail.com>
2022-10-19 16:39:11 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View>
+        <Text>The start of movie app project</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
